e1612f7571545f394fbf7a21517a1733f57fb812
Iris <irinaais90@yandex.ru>
2022-09-04 21:13:29 +0300

    feat: add models: user, movie

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const AuthError = require('../errors/AuthError');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: { validator: (v) => validator.isEmail(v) },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new AuthError('Неправильный логин или пароль');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new AuthError('Неправильный логин или пароль');
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
