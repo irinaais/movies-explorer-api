172663d886e3f9ed87ef0a8ff7b2a113f84dca04
Iris <irinaais90@yandex.ru>
2022-09-08 00:06:55 +0300

    refactor: controller/movies.js - migrate to async/await

--- controllers/movies.js
+++ controllers/movies.js
@@ -9,77 +9,76 @@ const {
   FORBIDDEN_ERROR_MESSAGE,
 } = require('../utils/constants');
 
-module.exports.sendMovies = (req, res, next) => {
-  Movie.find({ owner: req.user._id })
-    .then((movies) => {
-      if (!movies) {
-        throw new NotFoundError(NOT_SAVED_FILMS);
-      }
-      return res.send(movies);
-    })
-    .catch((err) => next(err));
+module.exports.sendMovies = async (req, res, next) => {
+  try {
+    const movies = await Movie.find({ owner: req.user._id });
+    if (!movies) {
+      throw new NotFoundError(NOT_SAVED_FILMS);
+    }
+    res.send(movies);
+  } catch (err) {
+    next(err);
+  }
 };
 
-module.exports.createMovie = (req, res, next) => {
-  const {
-    country,
-    director,
-    duration,
-    year,
-    description,
-    image,
-    trailerLink,
-    nameRU,
-    nameEN,
-    thumbnail,
-    movieId,
-  } = req.body;
-  const ownerId = req.user._id;
-  Movie.create({
-    country,
-    director,
-    duration,
-    year,
-    description,
-    image,
-    trailerLink,
-    nameRU,
-    nameEN,
-    thumbnail,
-    movieId,
-    owner: ownerId,
-  })
-    .then((movie) => {
-      if (!movie) {
-        throw new NotFoundError(FILM_NOT_FOUND);
-      }
-      return res.send(movie);
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new ValidationError(VALIDATION_ERROR_MESSAGE));
-      } else {
-        next(err);
-      }
+module.exports.createMovie = async (req, res, next) => {
+  try {
+    const {
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      nameRU,
+      nameEN,
+      thumbnail,
+      movieId,
+    } = req.body;
+    const ownerId = req.user._id;
+    const movie = await Movie.create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      nameRU,
+      nameEN,
+      thumbnail,
+      movieId,
+      owner: ownerId,
     });
+    if (!movie) {
+      throw new NotFoundError(FILM_NOT_FOUND);
+    }
+    res.send(movie);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      next(new ValidationError(VALIDATION_ERROR_MESSAGE));
+    } else {
+      next(err);
+    }
+  }
 };
 
-module.exports.deleteMovie = (req, res, next) => {
-  Movie.findById(req.params.movieId)
-    .then((movie) => {
-      if (movie == null || !movie) {
-        throw new NotFoundError(FILM_NOT_FOUND);
-      } else if (!movie.owner.equals(req.user._id)) {
-        throw new ForbiddenError(FORBIDDEN_ERROR_MESSAGE);
-      }
-      return movie.remove()
-        .then(() => res.send({ data: movie }));
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new ValidationError(VALIDATION_ERROR_MESSAGE));
-      } else {
-        next(err);
-      }
-    });
+module.exports.deleteMovie = async (req, res, next) => {
+  try {
+    const movie = await Movie.findById(req.params.movieId);
+    if (!movie) {
+      throw new NotFoundError(FILM_NOT_FOUND);
+    } else if (!movie.owner.equals(req.user._id)) {
+      throw new ForbiddenError(FORBIDDEN_ERROR_MESSAGE);
+    }
+    await movie.remove();
+    res.send({ data: movie });
+  } catch (err) {
+    if (err.name === 'CastError') {
+      next(new ValidationError(VALIDATION_ERROR_MESSAGE));
+    } else {
+      next(err);
+    }
+  }
 };
