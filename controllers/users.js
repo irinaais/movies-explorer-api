49dd6fd198a07a81b5fd90d2700ef30ed065f5c9
Iris <irinaais90@yandex.ru>
2022-09-08 01:34:05 +0300

    refactor: controller/users.js - migrate to async/await

--- controllers/users.js
+++ controllers/users.js
@@ -15,67 +15,67 @@ const {
 } = require('../utils/constants');
 const { getJwtToken } = require('../utils/utils');
 
-module.exports.createUser = (req, res, next) => {
-  const { name, email } = req.body;
-  bcrypt.hash(req.body.password, 10)
-    .then((hash) => User.create({
-      name, email, password: hash,
-    }))
-    .then((user) => User.findOne({ _id: user._id }))
-    .then((user) => {
-      res.send({ data: user });
-    })
-    .catch((err) => {
-      if (err.code === 11000) {
-        next(new ConflictError(CONFLICT_ERROR_MESSAGE));
-      } else if (err.name === 'ValidationError') {
-        next(new ValidationError(VALIDATION_ERROR_MESSAGE));
-      } else {
-        next(err);
-      }
-    });
-};
-
-module.exports.login = (req, res, next) => {
-  const { email, password } = req.body;
-  return User.findUserByCredentials(email, password)
-    .then((user) => {
-      const token = jwt.sign({ _id: user._id }, getJwtToken(), { expiresIn: '7d' });
-      res.send({ message: AUTH_SUCCESSFUL, token });
-    })
-    .catch((err) => {
-      if (err.message === 'IncorrectEmail') {
-        next(new AuthError(BAD_EMAIL_OR_PASSWORD));
-      }
+module.exports.createUser = async (req, res, next) => {
+  try {
+    const { name, email } = req.body;
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const user = await User.create({ name, email, password: hash });
+    const user2 = await User.findOne({ _id: user._id });
+    res.send({ data: user2 });
+  } catch (err) {
+    if (err.code === 11000) {
+      next(new ConflictError(CONFLICT_ERROR_MESSAGE));
+    } else if (err.name === 'ValidationError') {
+      next(new ValidationError(VALIDATION_ERROR_MESSAGE));
+    } else {
       next(err);
-    });
+    }
+  }
 };
 
-module.exports.getMe = (req, res, next) => {
-  User.findById(req.user._id)
-    .then((user) => {
-      if (!user) {
-        return next(new NotFoundError(USER_NOT_FOUND));
-      }
-      return res.send({ email: user.email, name: user.name });
-    })
-    .catch(next);
+module.exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findUserByCredentials(email, password);
+    const token = jwt.sign({ _id: user._id }, getJwtToken(), { expiresIn: '7d' });
+    res.send({ message: AUTH_SUCCESSFUL, token });
+  } catch (err) {
+    if (err.message === 'IncorrectEmail') {
+      next(new AuthError(BAD_EMAIL_OR_PASSWORD));
+    }
+    next(err);
+  }
 };
 
-module.exports.updateUser = (req, res, next) => {
-  const { name, email } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
-    .then((user) => {
-      if (!user) {
-        return next(new NotFoundError(USER_NOT_FOUND));
-      }
-      return res.send({ email: user.email, name: user.name });
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new ValidationError(VALIDATION_ERROR_MESSAGE));
-      } else {
-        next(err);
-      }
-    });
+module.exports.getMe = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      throw new NotFoundError(USER_NOT_FOUND);
+    }
+    res.send({ email: user.email, name: user.name });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updateUser = async (req, res, next) => {
+  try {
+    const { name, email } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { name, email },
+      { new: true, runValidators: true },
+    );
+    if (!user) {
+      throw new NotFoundError(USER_NOT_FOUND);
+    }
+    res.send({ email: user.email, name: user.name });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      next(new ValidationError(VALIDATION_ERROR_MESSAGE));
+    } else {
+      next(err);
+    }
+  }
 };
