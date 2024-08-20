0591762a7faedd41ec4a08e1bc2a46f45992528b
Irina Shorokhova <irinaais90@gmail.com>
2024-08-20 14:15:05 +0300

    fix: для подключения к MongoDB

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,2 @@
 module.exports.JWT_SECRET_DEV = 'dev-secret';
-module.exports.MONGO_URL_DEV = 'mongodb://localhost:27017/moviesdb';
+module.exports.MONGO_URL_DEV = 'mongodb://127.0.0.1:27017/moviesdb';
